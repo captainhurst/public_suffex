73aa3949a505e58899e242491d346c5a8d74e00b
Ryan Hurst <captainhurst@gmail.com>
2016-04-29 17:31:06 -0700

    get list and parse

--- lib/public_suffex.ex
+++ lib/public_suffex.ex
@@ -1,2 +1,30 @@
 defmodule PublicSuffex do
+  @path  "./data/public_suffix_list.dat.txt"
+
+  defmacro suffix(lines) do
+    
+  end
+  
+  def open_file! do
+    File.read!(@path)
+  end
+
+  def break_into_lines(file) do
+
+    String.split(file, "\n")
+  end
+
+  def strip_lines(lines) do
+    Enum.map(lines, fn (line) -> line |> String.strip end)
+  end
+
+  def strip_comments(lines) do
+    Enum.filter(lines, fn
+      ("//" <> _) -> false
+      ("//")      -> false
+      ("")        -> false
+      (_)         -> true
+    end)
+  end
+
 end
